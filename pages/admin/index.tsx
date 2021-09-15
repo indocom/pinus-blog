f2334ecd9bbc1adfe00be5996abdf553c887969d
Bernardus Krishna <krishnabernardus@gmail.com>
2021-09-16 00:49:59 +0800

    Add simple contentful (headless CMS) functionality. It renders the content of the CMS and changes accordingly upon editing.

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import 'tailwindcss/tailwind.css'
+import { createClient } from 'contentful'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-const Admin = () => {
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID!,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY!,
+  });
+
+  const res = await client.getEntries({ content_type: 'admin' });
+
+  return {
+    props: {
+      admin: res.items
+    }
+  }
+
+}
+
+const Admin = ({ admin } : { admin: any }) => {
+  console.log(admin);
   return (
     <div>
-      <text>
-        admin
-      </text>
+      {admin.map((post: any) => (
+        <div>
+          <div key={post.sys.id} className='text-3xl font-bold px-6 py-2'>{post.fields.title}</div>
+          <div className='px-6 py-2'>
+            {documentToReactComponents(post.fields.detail)}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
